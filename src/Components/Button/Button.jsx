7facfa7e1eb2fbb2e60973a77532b37d869f4201
Nikolai Tsumankov <Tsumankov86@gmail.com>
2023-11-30 12:54:03 +0500

    buger

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import "./Button.css"
 
 function Button({ type, title, disable, onClick }) {
-    function createRipple(event) {
-        const button = event.currentTarget;
+    /*    function createRipple(event) {
+    const button = event.currentTarget;
 
-        const circle = document.createElement("span");
-        const diameter = Math.max(button.clientWidth, button.clientHeight);
-        const radius = diameter / 2;
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
 
-        circle.style.width = circle.style.height = `${diameter}px`;
-        circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-        circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-        circle.classList.add("ripple");
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
 
-        const ripple = button.getElementsByClassName("ripple")[0];
+    const ripple = button.getElementsByClassName("ripple")[0];
 
-        if (ripple) {
-            ripple.remove();
-        }
-
-        button.appendChild(circle);
+    if (ripple) {
+        ripple.remove();
     }
+
+    button.appendChild(circle);
+}*/
     return (
         <button className={`btn ${(type === "add" && "add") ||
             (type === "remove" && "remove") ||
@@ -37,3 +37,4 @@ function Button({ type, title, disable, onClick }) {
 }
 
 export default Button
+
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,7 +4,7 @@ import Button from "../Button/Button";
 
 function Card({ food, onAdd, onRemove }) {
   const [count, setCount] = useState(0);
-  const { title, Image, price, id } = food;
+  const { title, Image, price } = food;
 
   const handleIncrement = () => {
     setCount(count + 1);
